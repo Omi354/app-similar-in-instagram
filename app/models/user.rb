499aed2b0c6af112cfd45b071a96169020610f1d
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-16 02:17:18 +0900

    postモデルを作成し、マイグレーション・modelファイルに関係性の記述、Controllerの作成、viewファイルの作成をしました

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
 
+  has_many :posts, dependent: :destroy
   has_one :profile, dependent: :destroy
 
 
