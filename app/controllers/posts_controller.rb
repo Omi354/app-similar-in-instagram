a28c3bf06fb86c5993e4ac52695265e4a6ee4ab9
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-17 02:33:15 +0900

    投稿画面を作成し、投稿できるようにしました。 また一覧画面を作成し、投稿の一覧を表示できるようにしました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
   def new
     @post = current_user.posts.build
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, notice: 'Post was successfully created.'
+    else
+      flash.now[:error] = 'Failed to create the post'
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:caption, images: [])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,8 @@
 #
 class Post < ApplicationRecord
   belongs_to :user
+  has_many_attached :images
+
+  validates :images, presence: true
+
 end
