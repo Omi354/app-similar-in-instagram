a681669c7a35b9c7fa21842304cab683e070f192
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-18 15:17:02 +0900

    commentモデルを作成し、commentsテーブルのマイグレーションとmodelファイルへの関係性の記述を行いました

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   has_many :likes, dependent: :destroy
-  has_many :liked_post, through: :liles, source: :post
+  has_many :liked_posts, through: :likes, source: :post
 
   has_one :profile, dependent: :destroy
 
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
