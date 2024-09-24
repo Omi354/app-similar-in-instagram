3d31078b813bbab311d5481e6f73e738e9b8b1b7
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-24 13:54:01 +0900

    ユーザー認証が必要なモデルに、  before_action :authenticate_user!　を追加しました タイムライン用のroutes,view,controllerを作成しました

--- app/controllers/unfollows_controller.rb
+++ app/controllers/unfollows_controller.rb
@@ -1,4 +1,6 @@
 class UnfollowsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     # accounts/showからpostリクエストがくる。follwer_id: :current_user.id, following_id: params[:account_id]　となるようなデータを作成
     following_relationship = current_user.following_relationships.find_by(following_id: params[:account_id])
