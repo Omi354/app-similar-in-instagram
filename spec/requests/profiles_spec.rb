560330229b07052e24cd58cf91479700e1ac1c1e
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-24 11:44:41 +0900

    プロフィールスペックを作成し、テストを実行しました

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -18,6 +18,48 @@ RSpec.describe "Profiles", type: :request do
       end
     end
 
+    describe "PUT /profile" do
+      let!(:avatar_path) { Rails.root.join('app/assets/images/default_avatar.png') }
+      let!(:avatar_file) { fixture_file_upload(avatar_path, 'image/png') }
+
+      it "200ステータス && 成功時のメッセージが取得される" do
+        put profile_path, params: { profile: { avatar: [avatar_file] } }
+        expect(response).to have_http_status(200)
+        body = JSON.parse(response.body)
+        expect(body['message']).to eq('Profile updated successfully')
+      end
+    end
+
+    describe "GET /profile/edit" do
+      it "200ステータス && hasAvatar: true && avatar_ulr が返ってくる" do
+        get edit_profile_path
+
+        expect(response).to have_http_status(200)
+
+        body = JSON.parse(response.body)
+        expect(body['hasAvatar']).to eq(true)
+        expect(body['avatarUrl']).to include('sample_avatar1.jpg')
+      end
+    end
   end
-  
+
+  context 'ログインしている && アバターが設定されていない場合' do
+    let!(:user) { create(:user) }
+
+    before do
+      sign_in user
+    end
+
+    describe "GET /profile/edit" do
+      it "200ステータス && hasAvatar: false が返ってくる" do
+        get edit_profile_path
+
+        expect(response).to have_http_status(200)
+
+        body = JSON.parse(response.body)
+        expect(body['hasAvatar']).to eq(false)
+      end
+    end
+  end
 end
+
